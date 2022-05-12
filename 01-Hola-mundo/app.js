80ac41389f629e40a47dae96b8b4374ef6eb5a2f
Javier Vargas - Ubuntu <javieravargasestudiante@gmail.com>
2022-05-12 15:23:04 -0500

    Primer commit de las bases de Js

---
+++ 01-Hola-mundo/app.js
@@ -0,0 +1,7 @@
+let nombre = "Javier Armando Vargas Vega"
+console.log (nombre)
+
+nombre = "Juan Sebastián Vargas Romero"
+console.log (nombre)
+
+console.log ('Hola mundo bello y hermoso. El programa más fácil.')
